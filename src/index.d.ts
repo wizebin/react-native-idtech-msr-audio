b42b71769e3b93af78d21ad5875d6e5b43b19e14
Stephen <wizebin@gmail.com>
2020-05-23 15:12:11 -0400

    Added podspec

---
+++ src/index.d.ts
@@ -0,0 +1,10 @@
+export declare const activate: any, deactivate: any, swipe: any;
+declare const _default: {
+    activate: any;
+    deactivate: any;
+    swipe: any;
+    parseSwipeData: any;
+    READERS: any;
+};
+export default _default;
+export * from './universal/index';
